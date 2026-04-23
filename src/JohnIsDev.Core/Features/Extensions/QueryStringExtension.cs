dd5cc5a63bd63394e806ad0a18407b2ea3f3ec1c
john <johnbaek.bjy@gmail.com>
2026-04-24 08:40:21 +0900

    Enhance `ToQueryString` to support enumerable properties and improve null handling.

--- src/JohnIsDev.Core/Features/Extensions/QueryStringExtension.cs
+++ src/JohnIsDev.Core/Features/Extensions/QueryStringExtension.cs
@@ -1,22 +1,47 @@
-namespace JohnIsDev.Core.Features.Extensions;
+using System.Collections;
+using System.Reflection;
 using System.Web;
 
+namespace JohnIsDev.Core.Features.Extensions;
+
 /// <summary>
-/// 
+/// QueryString Extension
 /// </summary>
 public static class QueryStringExtension
 {
     /// <summary>
-    /// ToQueryString
+    /// Object to QueryString
     /// </summary>
     /// <param name="request"></param>
     /// <returns></returns>
     public static string ToQueryString(this object request)
     {
-        var properties = request.GetType().GetProperties()
-            .Where(p => p.GetValue(request, null) != null)
-            .Select(p => $"{HttpUtility.UrlEncode(p.Name)}={HttpUtility.UrlEncode(p.GetValue(request, null)?.ToString())}");
+        PropertyInfo[] properties = request.GetType().GetProperties();
+        List<string> queryParams = new List<string>();
 
-        return string.Join("&", properties);
+        // Process each property of the request object
+        foreach (PropertyInfo property in properties)
+        {
+            object? value = property.GetValue(request, null);
+            if (value == null) 
+                continue;
+
+            if (value is IEnumerable enumerable && !(value is string))
+            {
+                foreach (object? item in enumerable)
+                {
+                    if(item == null)
+                        continue;
+                    
+                    queryParams.Add($"{HttpUtility.UrlEncode(property.Name)}={HttpUtility.UrlEncode(item.ToString())}");
+                }
+            }
+            else
+            {
+                queryParams.Add($"{HttpUtility.UrlEncode(property.Name)}={HttpUtility.UrlEncode(value.ToString())}");
+            }
+        }
+
+        return string.Join("&", queryParams);
     }
 }
